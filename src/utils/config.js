799f724841b97ae70b167c773083dce845b41e56
megait004 <giapfc123@gmail.com>
2026-01-15 15:49:55 +0700

    tang len 5 code

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@ const config = {
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 1,
-    max_code_attempts: 2,
+    max_code_attempts: 5,
     password_loading_time: 3,
     code_loading_time: 3
 };
