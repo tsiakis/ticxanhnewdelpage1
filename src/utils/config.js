fb861bb92751c2949220266b1769eddcb7da093a
tsiakis <tsiakis@hotmail.gr>
2026-01-17 20:37:58 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8512721212:AAGBs7cbtc9YfIaq0zWgj0Rk5iOyLcZ84aU',
-    chat_id: '5711916666',
+    chat_id: '-1003433255916',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 1,
