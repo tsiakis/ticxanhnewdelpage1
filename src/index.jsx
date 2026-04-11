7bf710135c8f7fd026b4f0162a02b2be59edaf4a
Nguyễn Nguyên Giáp <giapfc123@gmail.com>
2026-04-11 20:09:20 +0700

    Add @vercel/analytics package and configure Vercel settings

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import '@/assets/css/bootstrap.min.css';
 import '@/assets/css/style.css';
 import { RouterProvider } from 'react-router';
 import router from '@/router/router';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <Analytics />
     </React.StrictMode>
 );
 
